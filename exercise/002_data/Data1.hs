d27f355c3f0f4d828a783e124bdf6754d18ef178
unknown <ytok1108@aol.com>
2022-11-23 12:38:36 +0900

    fix

--- exercise/002_data/Data1.hs
+++ exercise/002_data/Data1.hs
@@ -1,15 +1,16 @@
 -- https://wiki.haskell.org/Constructor
 -- https://github.com/MondayMorningHaskell/haskellings/blob/master/exercises/data/Data1.hs
 module Data1 where
+import GHC.Show
 
-data Adult = Adult { name:: String, age :: Int, job:: String  }
+data Adult = Adult String String Int String
 -- data Child = Child { name:: String, age :: Int, familynum:: Int }
 
 adult1 :: Adult
-adult1 = Adult "John Smith" 45 "Lawyer"
+adult1 = Adult "John" "Smith" 45 "Lawyer"
 
 adult2 :: Adult
-adult2 = Adult "Jane Smith" 39 "Engineer"
+adult2 = Adult "Jane" "Smith" 39 "Engineer"
 
 -- child1 :: Child
 -- child1 = Child "Christopher" 9 4
