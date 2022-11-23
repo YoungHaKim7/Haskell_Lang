bd7a2a07f5f3c66e5fe00fda4e671f7472c10f46
unknown <ytok1108@aol.com>
2022-11-23 12:03:28 +0900

    Data__error code

---
+++ exercise/002_data/Data1.hs
@@ -0,0 +1,18 @@
+-- https://wiki.haskell.org/Constructor
+-- https://github.com/MondayMorningHaskell/haskellings/blob/master/exercises/data/Data1.hs
+module Data1 where
+
+data Adult = Adult { name:: String, age :: Int, job:: String  }
+-- data Child = Child { name:: String, age :: Int, familynum:: Int }
+
+adult1 :: Adult
+adult1 = Adult "John Smith" 45 "Lawyer"
+
+adult2 :: Adult
+adult2 = Adult "Jane Smith" 39 "Engineer"
+
+-- child1 :: Child
+-- child1 = Child "Christopher" 9 4
+-- 
+-- child2 :: Child
+-- child2 = Child "Stephanie" 12 6
