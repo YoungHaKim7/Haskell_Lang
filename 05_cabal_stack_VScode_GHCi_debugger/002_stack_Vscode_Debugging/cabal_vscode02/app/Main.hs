722e3e4b5ac932f24903bfd8bec2442d8455a9f1
GYoung <ytok1108@aol.com>
2023-10-04 22:08:26 +0900

    Cabal Debugging

---
+++ 05_cabal_stack_VScode_GHCi_debugger/002_stack_Vscode_Debugging/cabal_vscode02/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Sort
+
+main :: IO ()
+main = putStrLn $ show $ sort [6, 2, 4, 1, 9]
---
+++ 05_cabal_stack_VScode_GHCi_debugger/002_stack_Vscode_Debugging/cabal_vscode02/app/Sort.hs
@@ -0,0 +1,11 @@
+module Sort (sort) where
+
+sort :: (Ord a) => [a] -> [a]
+sort [] = []
+sort (x : xs) = insert x (sort xs)
+
+insert :: (Ord a) => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y : ys)
+  | x < y = x : y : ys
+  | otherwise = y : (insert x ys)
