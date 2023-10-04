bf13311581e526b639fedd3bec456fdc981dcaf2
GYoung <ytok1108@aol.com>
2023-10-04 22:11:39 +0900

    Cabal Debugging

---
+++ 05_cabal_stack_VScode_GHCi_debugger/002_stack_Vscode_Debugging/stack_vscode01/test/Spec.hs
@@ -0,0 +1,7 @@
+import Lib (pal)
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
+
+-- prop_reverseInvariant :: String -> Bool
+-- prop_reverseInvariant text = pal text == pal (reverse text)
